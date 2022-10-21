b8820f4ec988b33ba3eb2fa987728906d701d0e1
Abdullah KUŞ <70553662+abdullahkus@users.noreply.github.com>
2022-10-21 18:13:20 +0300

    Line edited
    Line first and last items rounded js to css

--- src/components/Race/RaceLane.vue
+++ src/components/Race/RaceLane.vue
@@ -4,22 +4,17 @@ import RaceHorse from './RaceHorse.vue';
 </script>
 
 <template>
-  <!-- Lanes -->
-  <div v-for="(number, index) in 8" :key="index" class="race__lane"
-    :class="[(number % 2) ? 'race__lane--dark' : 'race__lane--light'], {'race__lane--first' : (number == 1)}, {'race__lane--last' : (number == 8)} ">
+  <div class="race__lanes">
+    <div v-for="(number, index) in 8" :key="index" class="race__lane"
+      :class="[(number % 2) ? 'race__lane--dark' : 'race__lane--light']">
 
-    <!-- Horse -->
-    <RaceHorse :style="'left: 25%' ">
-    </RaceHorse>
+      <RaceHorse :style="'left: 25%' "></RaceHorse>
 
-    <!-- Start Point -->
-    <div class="race__point"></div>
+      <div class="race__point"></div>
 
-    <!-- Finish Point -->
-    <div class="race__point race__point--finish"></div>
-
+      <div class="race__point race__point--finish"></div>
+    </div>
   </div>
-
 </template>
 
 <style lang="scss">
