f2733e92e6752b81a23897df2e822abf68fd3df9
Abdullah KUŞ <70553662+abdullahkus@users.noreply.github.com>
2022-10-23 18:43:20 +0300

    RaceInformation component delete

--- src/components/Race/RaceInterface.vue
+++ src/components/Race/RaceInterface.vue
@@ -1,12 +1,19 @@
 <script setup>
+import RaceHorseForm from './RaceHorseForm.vue';
 import RaceInformation from './RaceInformation.vue';
+import RaceLeaderboard from './RaceLeaderboard.vue';
+import RaceResult from './RaceResult.vue';
 
+import { useRaceStore } from "@/stores/raceStore"
 
+const raceStore = useRaceStore()
 </script>
 
 <template>
-  <div class="race__interface">
-    <RaceInformation></RaceInformation>
+  <div class="race__interface ">
+    <RaceHorseForm v-if="raceStore.showHorseForm"></RaceHorseForm>
+    <RaceLeaderboard v-if="raceStore.showLeaderboard"></RaceLeaderboard>
+    <RaceResult v-if="raceStore.showResult"></RaceResult>
   </div>
 </template>
 
