d93d112e13a9236705703f78bf07d898ad679ee1
Abdullah KUŞ <70553662+abdullahkus@users.noreply.github.com>
2022-10-27 13:44:17 +0300

    Horse name migrate store

--- src/components/Race/RaceHorseForm.vue
+++ src/components/Race/RaceHorseForm.vue
@@ -1,15 +1,22 @@
 <script setup>
+import { ref, onMounted, watch } from 'vue'
 import Button from '../Button.vue';
 import InputText from '../Input/InputText.vue';
 import { useRaceStore } from "@/stores/raceStore"
 
+
 const emits = defineEmits(["start"])
+
+const raceStore = useRaceStore()
+
+
 
 function start() {
   emits("start")
 }
 
-const raceStore = useRaceStore()
+
+
 </script>
 
 <template>
@@ -17,7 +24,7 @@ const raceStore = useRaceStore()
     <h2 class="race__title">Horse Names</h2>
     <ol class="race__list">
       <li v-for="horse in raceStore.horses" :key="horse.id" class="race__list-item lane-marker">
-        <InputText></InputText>
+        <InputText v-model:name="horse.name"></InputText>
       </li>
     </ol>
     <div class="race__buttons">
